07b001058679097d92b329ddb74487941c418201
Vikram <vikramsingh001@gmail.com>
2026-03-27 17:31:03 +0100

    Add git blame (#86)
    * Add git blame
    
    * Switch modes

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,6 +21,7 @@
 -- Git (Diffview & LazyGit):
 --   <leader>gs : Open Diffview (against index/HEAD)
 --   <leader>gm : Open Diffview (against main or master branch)
+--   <leader>gb : Show Git blame for current line
 --   <leader>gS : Close Diffview & cleanup buffers
 --   <leader>gM : Close Diffview & cleanup buffers
 --   <leader>gh : File History (Diffview)
@@ -138,8 +139,8 @@ end
 --
 ----  See `:help wincmd` for a list of all window commands
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-j>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-k>", { desc = "Move focus to the lower window" })
 -- Does not seem to work
 vim.keymap.set("n", "<C-b><Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
@@ -336,6 +337,13 @@ require("lazy").setup({
 			notifier = { enabled = true },
 		},
 		keys = {
+			{
+				"<leader>gb",
+				function()
+					Snacks.git.blame_line()
+				end,
+				desc = "Git Blame Line",
+			},
 			{
 				"<leader>sh",
 				function()
@@ -1159,7 +1167,7 @@ require("lazy").setup({
 
 			require("diffview").setup({
 				file_panel = {
-					listing_style = "list",
+					listing_style = "tree",
 					win_config = {
 						position = "left",
 						width = diffview_collapsed_width,
